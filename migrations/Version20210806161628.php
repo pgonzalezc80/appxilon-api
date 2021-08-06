0b36219022f3859bdc05523e00110f8289e3cfe3
Pablo Gonzalez <pgonzalezc80@gmail.com>
2021-08-06 20:51:17 +0200

    [DEV] Creating tasks api rest

--- migrations/Version20210806161628.php
+++ migrations/Version20210806161628.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210805200433 extends AbstractMigration
+final class Version20210806161628 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210805200433 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE task ADD status TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE task');
+        $this->addSql('ALTER TABLE task DROP status');
     }
 }
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -2,13 +2,29 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TaskRepository;
+use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"="task:read"},
+ *     denormalizationContext={"groups"="task:write"},
+ *     attributes={
+ *          "pagination_items_per_page"=3,
+ *          "formats"={"jsonld","json","html","jsonhal","csv"={"text/csv"}}
+ *     }
+ * )
  * @ORM\Entity(repositoryClass=TaskRepository::class)
+ * @ApiFilter(BooleanFilter::class,properties={"status"})
+ * @ApiFilter(SearchFilter::class,properties={"title":"partial"})
  */
 class Task
 {
@@ -20,14 +36,42 @@ class Task
     private $id;
 
     /**
+     * Title for task
+     * @Groups({"task:write","task:read","user:read"})
+     * @Assert\NotBlank()
      * @ORM\Column(type="string", length=255)
      */
     private $title;
 
     /**
-     * @ORM\Column(type="text", nullable=true)
+     * @Assert\NotBlank()
+     * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     *
+     */
+    private $createdAt;
+
+    /**
+     * Indicates if the task is completed or not
+     * @Groups({"task:write","task:read","user:read"})
+     * @ORM\Column(type="boolean")
+     */
+    private $status = false;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tasks")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"task:write","task:read"})
+     */
+    private $owner;
+
+    public function __construct(){
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -46,14 +90,62 @@ class Task
         return $this;
     }
 
+    /**
+     * @return string|null
+     * @Groups({"task:read"})
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(?string $description): self
+    /**
+     * @param string|null $description
+     * @return $this
+     * @Groups({"task:write"})
+     * @SerializedName("description")
+     */
+    public function setTextDescription(?string $description): self
     {
-        $this->description = $description;
+        $this->description = nl2br($description);
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string
+     * @Groups({"task:read","user:read"})
+     */
+    public function getCreatedAtAgo():string
+    {
+        return Carbon::instance($this->getCreatedAt())->diffForHumans();
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        $this->owner = $owner;
 
         return $this;
     }
